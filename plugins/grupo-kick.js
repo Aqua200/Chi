d3d0fd8a59ab9563ab76fbc430237a36439ded98
Aqua200 <chinquepapa@gmail.com>
2025-02-13 03:34:59 -0800

    Update grupo-kick.js

--- plugins/grupo-kick.js
+++ plugins/grupo-kick.js
@@ -1,9 +1,11 @@
 let handler = async (m, { conn, participants, usedPrefix, command, isROwner }) => {
-let kickte = `A quien eliminó? etiquetas a una persona con @tag pendejo`
-if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
-let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-let owr = m.chat.split`-`[0]
-await conn.groupParticipantsUpdate(m.chat, [user], 'remove')}
+  let kickte = `☆彡 ¿A quién quieres eliminar, cariño? ☆彡\nPor favor, etiqueta a una persona con @tag, ¡y sé más cuidadoso! (｡♥‿♥｡)`
+  if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte) }) 
+  let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
+  let owr = m.chat.split`-`[0]
+  await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
+  conn.sendMessage(m.chat, `*~(っ˘̩╭╮˘̩)っ* ¡Oh no, @${user.split('@')[0]}! 😢💔\nLo siento mucho, pero ¡te tengo que sacar del grupo! Espero que no te enojes, te mando muchos abrazos. 💕✨`, { mentions: conn.parseMention(`*~(っ˘̩╭╮˘̩)っ* ¡Oh no, @${user.split('@')[0]}! 😢💔\nLo siento mucho, pero ¡te tengo que sacar del grupo! Espero que no te enojes, te mando muchos abrazos. 💕✨`) })
+}
 handler.help = ['kick *@user*']
 handler.tags = ['group']
 handler.command = ['kick', 'expulsar'] 
