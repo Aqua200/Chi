c1394ae507490a96b300371688c248756596de93
Aqua200 <chinquepapa@gmail.com>
2025-02-13 03:51:06 -0800

    Update grupo-kick.js

--- plugins/grupo-kick.js
+++ plugins/grupo-kick.js
@@ -1,26 +1,29 @@
 let handler = async (m, { conn, participants, usedPrefix, command, isROwner }) => {
   let kickte = `☆彡 ¿A quién quieres eliminar, cariño? ☆彡
-Por favor, etiqueta a una persona con @tag, ¡y sé más cuidadoso! (｡♥‿♥｡)`
+Por favor, etiqueta a una persona con @tag, ¡y sé más cuidadoso! (｡♥‿♥｡)`;
   
   if (!m.mentionedJid[0] && !m.quoted) {
-    return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte) })
+    return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte) });
   }
   
-  let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-  await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
+  let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
   
-  // Se envía el mensaje en formato objeto, utilizando { text: mensaje }
+  // Se elimina al usuario de inmediato
+  await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
+  
+  // Se prepara el mensaje tierno, y se integran el texto y las menciones en un solo objeto
   let message = `*~(っ˘̩╭╮˘̩)っ* ¡Oh no, cariño! 😢💔
-Lo siento mucho, pero ¡te tengo que sacar del grupo! Espero que no te enojes, te mando muchos abrazos. 💕✨`
+Lo siento mucho, pero ¡te tengo que sacar del grupo! Espero que no te enojes, te mando muchos abrazos. 💕✨`;
   
-  conn.sendMessage(m.chat, { text: message }, { mentions: [user] })
+  await conn.sendMessage(m.chat, { text: message, mentions: [user] });
 }
 
-handler.help = ['kick *@user*']
-handler.tags = ['group']
-handler.command = ['kick', 'expulsar']
-handler.admin = true
-handler.group = true
-handler.botAdmin = true
-handler.register = true 
-export default handler
+handler.help = ['kick *@user*'];
+handler.tags = ['group'];
+handler.command = ['kick', 'expulsar'];
+handler.admin = true;
+handler.group = true;
+handler.botAdmin = true;
+handler.register = true;
+
+export default handler;
