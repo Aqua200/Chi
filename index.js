f06937cbe66be764e99ce9d094050b3eb1cad4df
Aqua200 <chinquepapa@gmail.com>
2025-02-14 03:53:21 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,113 +1,109 @@
-console.log('Iniciando 🚀🚀🚀') 
-import { join, dirname } from 'path'
+console.log('Iniciando 🚀🚀🚀'); 
+import { join, dirname } from 'path';
 import { createRequire } from 'module';
-import { fileURLToPath } from 'url'
-import { setupMaster, fork } from 'cluster'
-import { watchFile, unwatchFile } from 'fs'
+import { fileURLToPath } from 'url';
+import { fork } from 'cluster';
+import { watchFile, unwatchFile } from 'fs';
 import cfonts from 'cfonts';
-import { createInterface } from 'readline'
-import yargs from 'yargs'
-import express from 'express'
-import chalk from 'chalk'
-import path from 'path'
-import os from 'os'
-import { promises as fsPromises } from 'fs'
+import { createInterface } from 'readline';
+import yargs from 'yargs';
+import chalk from 'chalk';
+import os from 'os';
+import { promises as fsPromises } from 'fs';
 
-// https://stackoverflow.com/a/50052194
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(__dirname) //Incorpora la capacidad de crear el método 'requerir'
-const { name, author } = require(join(__dirname, './package.json')) //https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
-const { say } = cfonts
-const rl = createInterface(process.stdin, process.stdout)
-
-//const app = express()
-//const port = process.env.PORT || 8080;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const require = createRequire(import.meta.url); 
+const { name, author } = require(join(__dirname, './package.json')); 
+const { say } = cfonts;
+const rl = createInterface(process.stdin, process.stdout);
 
 say('China-Mitzuki-Bot-MD', {
-font: 'chrome',
-align: 'center',
-gradient: ['red', 'magenta']})
+  font: 'chrome',
+  align: 'center',
+  gradient: ['red', 'magenta']
+});
 say(`Bot personalizado`, {
-font: 'console',
-align: 'center',
-gradient: ['red', 'magenta']})
+  font: 'console',
+  align: 'center',
+  gradient: ['red', 'magenta']
+});
 
-var isRunning = false
+var isRunning = false;
 
 async function start(file) {
-if (isRunning) return
-isRunning = true
-const currentFilePath = new URL(import.meta.url).pathname
-let args = [join(__dirname, file), ...process.argv.slice(2)]
-say([process.argv[0], ...args].join(' '), {
-font: 'console',
-align: 'center',
-gradient: ['red', 'magenta']
-})
-setupMaster({exec: args[0], args: args.slice(1),
-})
-let p = fork()
-p.on('message', (data) => {
-console.log('╭--------- - - - ✓\n┆ ✅ TIEMPO DE ACTIVIDAD ACTUALIZADA\n╰-------------------- - - -', data)
-switch (data) {
-case 'reset':
-p.process.kill()
-isRunning = false
-start.apply(this, arguments)
-break
-case 'uptime':
-p.send(process.uptime())
-break
-}})
+  if (isRunning) return;
+  isRunning = true;
 
-p.on('exit', (_, code) => {
-isRunning = false
-console.error('⚠️ ERROR ⚠️ >> ', code)
-start('main.js'); //
+  let args = [join(__dirname, file), ...process.argv.slice(2)];
 
-if (code === 0) return
-watchFile(args[0], () => {
-unwatchFile(args[0])
-start(file)
-})})
+  say([process.argv[0], ...args].join(' '), {
+    font: 'console',
+    align: 'center',
+    gradient: ['red', 'magenta']
+  });
 
-const ramInGB = os.totalmem() / (1024 * 1024 * 1024)
-const freeRamInGB = os.freemem() / (1024 * 1024 * 1024)
-const packageJsonPath = path.join(path.dirname(currentFilePath), './package.json')
-try {
-const packageJsonData = await fsPromises.readFile(packageJsonPath, 'utf-8')
-const packageJsonObj = JSON.parse(packageJsonData)
-const currentTime = new Date().toLocaleString()
-let lineM = '⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ 》'
-console.log(chalk.yellow(`╭${lineM}
-┊${chalk.blueBright('╭┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}
-┊${chalk.blueBright('┊')}${chalk.yellow(`🖥️ ${os.type()}, ${os.release()} - ${os.arch()}`)}
-┊${chalk.blueBright('┊')}${chalk.yellow(`💾 Total RAM: ${ramInGB.toFixed(2)} GB`)}
-┊${chalk.blueBright('┊')}${chalk.yellow(`💽 Free RAM: ${freeRamInGB.toFixed(2)} GB`)}
-┊${chalk.blueBright('╰┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}
-┊${chalk.blueBright('╭┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}
-┊${chalk.blueBright('┊')} ${chalk.blue.bold(`🟢INFORMACIÓN :`)}
-┊${chalk.blueBright('┊')} ${chalk.blueBright('┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')} 
-┊${chalk.blueBright('┊')}${chalk.cyan(`💚 Nombre: ${packageJsonObj.name}`)}
-┊${chalk.blueBright('┊')}${chalk.cyan(`𓃠 Versión: ${packageJsonObj.version}`)}
-┊${chalk.blueBright('┊')}${chalk.cyan(`💜 Descripción: ${packageJsonObj.description}`)}
-┊${chalk.blueBright('┊')}${chalk.cyan(`💕 Dueña : 𝕮𝖍𝖎𝖓𝖆 𝕸𝖎𝖙𝖟𝖚𝖐𝖎 💋`)}
-┊${chalk.blueBright('┊')}${chalk.cyan(`ღ Project Author, bot personalizado echo por:  ${packageJsonObj.author.name}`)}
-┊${chalk.blueBright('┊')}${chalk.blueBright('┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')} 
-┊${chalk.blueBright('╭┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}
-┊${chalk.blueBright('┊')}${chalk.cyan(`⏰ Hora Actual :`)}
-┊${chalk.blueBright('┊')}${chalk.cyan(`${currentTime}`)}
-┊${chalk.blueBright('╰┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')} 
-╰${lineM}`));
-setInterval(() => {}, 1000)
-} catch (err) {
-console.error(chalk.red(`❌ No se pudo leer el archivo package.json: ${err}`))
+  let p = fork(args[0], args.slice(1));
+
+  p.on('message', (data) => {
+    console.log('╭--------- - - - ✓\n┆ ✅ TIEMPO DE ACTIVIDAD ACTUALIZADA\n╰-------------------- - - -', data);
+    switch (data) {
+      case 'reset':
+        p.kill();
+        isRunning = false;
+        start(file);
+        break;
+      case 'uptime':
+        p.send(process.uptime());
+        break;
+    }
+  });
+
+  p.on('exit', (code) => {
+    isRunning = false;
+    console.error('⚠️ ERROR ⚠️ >> ', code);
+    if (code !== 0) start('main.js');
+  });
+
+  watchFile(args[0], () => {
+    unwatchFile(args[0]);
+    start(file);
+  });
+
+  const ramInGB = os.totalmem() / (1024 * 1024 * 1024);
+  const freeRamInGB = os.freemem() / (1024 * 1024 * 1024);
+  
+  try {
+    const packageJsonData = await fsPromises.readFile(join(__dirname, './package.json'), 'utf-8');
+    const packageJsonObj = JSON.parse(packageJsonData);
+    const currentTime = new Date().toLocaleString();
+
+    console.log(chalk.yellow(`
+╭──────────────
+┊ 🖥️ ${os.type()}, ${os.release()} - ${os.arch()}
+┊ 💾 RAM Total: ${ramInGB.toFixed(2)} GB
+┊ 💽 RAM Libre: ${freeRamInGB.toFixed(2)} GB
+┊ 🟢 Información:
+┊ 💚 Nombre: ${packageJsonObj.name}
+┊ 𓃠 Versión: ${packageJsonObj.version}
+┊ 💜 Descripción: ${packageJsonObj.description}
+┊ 💕 Dueña : 𝕮𝖍𝖎𝖓𝖆 𝕸𝖎𝖙𝖟𝖚𝖐𝖎 💋
+┊ ღ Autor del proyecto: ${packageJsonObj.author.name}
+┊ ⏰ Hora Actual: ${currentTime}
+╰──────────────`));
+
+    setInterval(() => {}, 1000);
+  } catch (err) {
+    console.error(chalk.red(`❌ No se pudo leer el archivo package.json: ${err}`));
+  }
+
+  let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
+  if (!opts['test']) {
+    if (!rl.listenerCount('line')) {
+      rl.on('line', line => {
+        p.emit('message', line.trim());
+      });
+    }
+  }
 }
 
-let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
-if (!opts['test'])
-if (!rl.listenerCount()) rl.on('line', line => {
-p.emit('message', line.trim())
-})}
-
-start('main.js')
+start('main.js');
